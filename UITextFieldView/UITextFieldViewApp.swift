15184274ef6e5a5b5c10c6cf11ab3d720663739b
Insub Kim <124117176+insub4974@users.noreply.github.com>
2023-03-15 21:08:04 +0900

    Initial Commit

---
+++ UITextFieldView/UITextFieldViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITextFieldViewApp.swift
+//  UITextFieldView
+//
+//  Created by insub on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct UITextFieldViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
